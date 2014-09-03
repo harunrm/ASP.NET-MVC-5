b9f9841db92eaaca4df1afa4e315d94fddd4a26f
harunrm <harungeo@yahoo.com>
2014-09-03 14:40:09 +0600

    Circle Unit Test

---
+++ CircleApp/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace CircleApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtPerimeter = new System.Windows.Forms.TextBox();
+            this.txtDiameter = new System.Windows.Forms.TextBox();
+            this.texArea = new System.Windows.Forms.TextBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Radius";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Perimeter";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Area";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(31, 169);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(45, 13);
+            this.label.TabIndex = 0;
+            this.label.Text = "Diamiter";
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Location = new System.Drawing.Point(93, 42);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(100, 20);
+            this.txtRadius.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(93, 123);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // txtPerimeter
+            // 
+            this.txtPerimeter.Location = new System.Drawing.Point(93, 80);
+            this.txtPerimeter.Name = "txtPerimeter";
+            this.txtPerimeter.Size = new System.Drawing.Size(100, 20);
+            this.txtPerimeter.TabIndex = 1;
+            // 
+            // txtDiameter
+            // 
+            this.txtDiameter.Location = new System.Drawing.Point(93, 166);
+            this.txtDiameter.Name = "txtDiameter";
+            this.txtDiameter.Size = new System.Drawing.Size(100, 20);
+            this.txtDiameter.TabIndex = 1;
+            // 
+            // texArea
+            // 
+            this.texArea.Location = new System.Drawing.Point(93, 123);
+            this.texArea.Name = "texArea";
+            this.texArea.Size = new System.Drawing.Size(100, 20);
+            this.texArea.TabIndex = 1;
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(215, 42);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 2;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 261);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.texArea);
+            this.Controls.Add(this.txtDiameter);
+            this.Controls.Add(this.txtPerimeter);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtRadius);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtPerimeter;
+        private System.Windows.Forms.TextBox txtDiameter;
+        private System.Windows.Forms.TextBox texArea;
+        private System.Windows.Forms.Button showButton;
+    }
+}
+
---
+++ CircleApp/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CircleTest;
+
+namespace CircleApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Circle aCircle;
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            double radius = Convert.ToDouble(txtRadius.Text);
+            aCircle=new Circle(radius);
+            texArea.Text = aCircle.GetPerimeter().ToString();
+            label.Text = aCircle.GetArea().ToString();
+            txtPerimeter.Text = aCircle.GetDiameter().ToString();
+        }
+    }
+}
